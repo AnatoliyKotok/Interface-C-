8ed1998ba5b18138d9a38c309fee935395747785
AnatoliyKotok <anatolijkotok@gmail.com>
2020-10-01 18:15:22 +0300

    first commit

---
+++ Drink_hw/Drink.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Drink_hw
+{
+    class Drink: IComparable, IComparable<Drink>
+    {
+        
+        public enum TypeDrink { HotDrinc = 0, ColdDrinc, WithGas, WithoutGas };
+        TypeDrink type = TypeDrink.ColdDrinc;
+        
+
+        public Drink(string name, uint call, uint price,string producer, TypeDrink drink)
+        {
+            this.Name = name;
+            this.Call = call;
+            this.Price = price;
+            this.Producer = producer;
+            this.type = drink;
+        }
+        public string Name { 
+            get;
+            set; 
+        }
+        public uint Call { get; set; }
+        public uint Price { get; set; }
+        public string Producer { get; set; }
+        public override string ToString()
+        {
+            return $"{Name}\t{type}\t{Call}\t{Price}\t{Producer}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+
+                throw new ArgumentNullException("Other object is null");
+            if (!(obj is Drink))
+                throw new ArgumentNullException("other object not Car");
+            Drink drink = (Drink)obj;
+            return this.type.CompareTo(drink.type);
+        }
+
+        public int CompareTo( Drink other)
+        {
+            return this.Name.CompareTo(other.Name); ;
+        }
+    }
+    class CmpPrice :IComparer<Drink>,IComparer
+    {
+        
+
+        public int Compare(object x, object y)
+        {
+            if (x == null || y == null)
+            {
+                throw new ArgumentNullException("Object is null");
+            }
+            if (!(x is Drink) || !(y is Drink))
+            {
+                throw new ArgumentNullException("Is not car");
+            }
+            Drink d1 = (Drink)x;
+            Drink d2 = (Drink)y;
+
+            return d1.Price.CompareTo(d2.Price);
+        }
+
+        public int Compare([AllowNull] Drink x, [AllowNull] Drink y)
+        {
+            return x.Price.CompareTo(y.Price);
+        }
+
+        
+    }
+    class CmpCall : IComparer<Drink>,IComparer
+    {
+
+
+        public int Compare(object x, object y)
+        {
+            if (x == null || y == null)
+            {
+                throw new ArgumentNullException("Object is null");
+            }
+            if (!(x is Drink) || !(y is Drink))
+            {
+                throw new ArgumentNullException("Is not car");
+            }
+            Drink d1 = (Drink)x;
+            Drink d2 = (Drink)y;
+
+            return -d1.Call.CompareTo(d2.Call);
+        }
+
+        public int Compare([AllowNull] Drink x, [AllowNull] Drink y)
+        {
+            return -x.Price.CompareTo(y.Price);
+        }
+
+    }
+    class CmpProd :IComparer<Drink>, IComparer
+    {
+
+
+        public int Compare(object x, object y)
+        {
+            if (x == null || y == null)
+            {
+                throw new ArgumentNullException("Object is null");
+            }
+            if (!(x is Drink) || !(y is Drink))
+            {
+                throw new ArgumentNullException("Is not car");
+            }
+            Drink d1 = (Drink)x;
+            Drink d2 = (Drink)y;
+
+            return d1.Producer.CompareTo(d2.Producer);
+        }
+        public int Compare([AllowNull] Drink x, [AllowNull] Drink y)
+        {
+            return x.Price.CompareTo(y.Price);
+        }
+    }
+}
---
+++ Drink_hw/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Drink_hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drink whoter = new Drink("Morsh", 1, 15, "Obolon", Drink.TypeDrink.WithoutGas);
+            Console.WriteLine(whoter);
+            Drink juce = new Drink("Jafa", 2, 34, "Jafa", Drink.TypeDrink.WithoutGas);
+            List<Drink> drinks = new List<Drink>{
+                new Drink("Pepsi",5,21,"Pepsi",Drink.TypeDrink.WithGas),
+                new Drink("Cola", 12, 20, "Cola", Drink.TypeDrink.WithGas),
+                new Drink("Sadochok",2,25,"Sadochok",Drink.TypeDrink.WithoutGas)
+
+            };
+            Console.WriteLine("Sorted by price");
+            drinks.Sort( new CmpPrice());
+            foreach (var d in drinks)
+            {
+                Console.WriteLine(d);
+            }
+            
+            Console.WriteLine("After comparer by call");
+            drinks.Sort(new CmpCall());
+            foreach (var item in drinks)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("After comparer by producer");
+            drinks.Sort(new CmpProd());
+            foreach (var item in drinks)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            ArrayList li = new ArrayList()
+            { whoter,juce};
+            Console.WriteLine("Sort arrliat by price");
+            li.Sort(new CmpPrice());
+            for (int i = 0; i < li.Count; i++)
+            {
+                Console.WriteLine(li[i]);
+            }
+            
+            
+            Console.WriteLine("Sort arrliat by call");
+            li.Sort(new CmpCall());
+            for (int i = 0; i < li.Count; i++)
+            {
+                Console.WriteLine(li[i]);
+            }
+            
+            
+            Console.WriteLine("Sort arrliat by producer");
+            li.Sort(new CmpProd());
+            for (int i = 0; i < li.Count; i++)
+            {
+                Console.WriteLine(li[i]);
+            }
+        }
+    }
+}
